bbe1d7e6f30a21a8f20904635430baa382b1b1cd
Theodor Diesner-Mayer <theo.dm94@gmail.com>
2023-11-25 16:01:59 +0100

    Einige Tests mehr

--- src/notebooks/test.py
+++ src/notebooks/test.py
@@ -4,12 +4,12 @@ from src.search.strategy.random_walker_1 import RandomWalker1StrategyParams
 from src.torus_creation.random_grid import RandomNormalStrategyParams
 
 res = simulate(
-    graph_strategy="random_normal",
+    graph_strategy="random",
     graph_stratey_params=RandomNormalStrategyParams.default(100),
     grid_width=40,
     grid_height=40,
     num_distinct_information=100,
-    random_walker_strategy="only_random_walker",
+    random_walker_strategy="random_walker_1",
     random_walker_strategy_params=RandomWalker1StrategyParams(
         random_probability=0.9,
         random_probability_of_adding_edge=0.005,
